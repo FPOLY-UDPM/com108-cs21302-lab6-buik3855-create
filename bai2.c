3ca43e0a2bc6927fe09098303bf3fa65495f9397
buik3855-create <buik3855@gmail.com>
2026-02-24 10:38:08 +0700

    dangkhoa nop lap6

--- bai2.c
+++ bai2.c
@@ -1,12 +1,42 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [BÙI ĐĂNG KHOA]
+ * MSSV:      [PS48500]
+ * Lớp:       [CS21302]
  *****************************************************************************/
 
 //  BÀI 2: TÌM GIÁ TRỊ LỚN NHẤT VÀ NHỎ NHẤT TRONG MẢNG 
 //  Input: Nhập vào từ bàn phím 1 mảng các số nguyên. Mảng gồm n phần tử 
 //  Output: Xuất ra màn hình giá trị nhỏ nhất và lớn nhất của mảng
 
-// VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <limits.h>
+                         
+int main() {
+    int n;
+    printf("Nhap so luong phan tu trong mang: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Nhap cac phan tu trong mang:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = INT_MIN;
+    int min = INT_MAX;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    printf("Gia tri lon nhat trong mang: %d\n", max);
+    printf("Gia tri nho nhat trong mang: %d\n", min);
+
+    return 0;
+}
 
